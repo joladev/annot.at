a5a966313b2dd241c0cfbfeb9d19f689c70aa283
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2026-09-04 18:26:00 +0100

    Bump Elixir dependencies

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule AnnotAt.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.2.0"},
+      {:dns_cluster, "~> 0.3.0"},
       {:bandit, "~> 1.5"},
       {:jose, "~> 1.11"},
       {:credo, "~> 1.7", only: [:dev, :test]},
